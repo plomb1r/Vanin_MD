af52b84ae0ebf2e4d97da8b1c2d5cbd451c364e8
plomb1r <vanin.vlad@inbox.ru>
2022-03-03 03:40:48 +0300

    Расчет потенциалов Л-Дж., силы взаимодействия

--- global_var.h
+++ global_var.h
@@ -1,26 +1,25 @@
 ﻿#pragma once
 #pragma region Кооординаты (массивы от числа частиц, где i - номер частицы)
-double* coordx = (double*)malloc(NUMBERPARTICLES * sizeof(double)); // координаты
+double* coordx = (double*)malloc(NUMBERPARTICLES * sizeof(double)); 
 double* coordy = (double*)malloc(NUMBERPARTICLES * sizeof(double));
 double* coordz = (double*)malloc(NUMBERPARTICLES * sizeof(double));
 #pragma endregion
 
 #pragma region Скорости (массивы от числа частиц, где i - номер частицы)
-double* vx = (double*)malloc(NUMBERPARTICLES * sizeof(double));	 // скорости 
+double* vx = (double*)malloc(NUMBERPARTICLES * sizeof(double));	 
 double* vy = (double*)malloc(NUMBERPARTICLES * sizeof(double));
 double* vz = (double*)malloc(NUMBERPARTICLES * sizeof(double));
-
 #pragma endregion
 
-// double U потенциальная энергия(скаляр)
-
+#pragma region Потенциал Леннарда-Джонса
+double U;
+#pragma endregion
 
 #pragma region Силы взаимодействия (массивы от числа частиц и вспомогательный скаляр)
- /*
-double* Fx = (double*)malloc(NUMBERPARTICLES * sizeof(double)); // силы
+double* Fx = (double*)malloc(NUMBERPARTICLES * sizeof(double)); 
 double* Fy = (double*)malloc(NUMBERPARTICLES * sizeof(double));
 double* Fz = (double*)malloc(NUMBERPARTICLES * sizeof(double));
- */
  
 #pragma endregion
+double F;
  ;
